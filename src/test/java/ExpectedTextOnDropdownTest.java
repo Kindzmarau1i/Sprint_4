145b989803dba66ec266f397c9ab93c81a3b0382
Danil Agliullin <danil.agliullin@rt.ru>
2024-09-19 19:21:00 +0300

    Добавил базовый класс

--- src/test/java/ExpectedTextOnDropdownTest.java
+++ src/test/java/ExpectedTextOnDropdownTest.java
@@ -1,17 +1,11 @@
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import samokatPageObject.MainPage;
 
-public class ExpectedTextOnDropdownTest {
+public class ExpectedTextOnDropdownTest extends BaseTest {
 
     @Test
     public void checkForExpectedTextOnDropdown() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
-
         MainPage mainPage = new MainPage(driver);
         mainPage.checkTextOnDropdown("Сутки — 400 рублей. Оплата курьеру — наличными или картой.");
-        driver.quit();
     }
 }
--- src/test/java/SuccessOrderTest.java
+++ src/test/java/SuccessOrderTest.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import samokatPageObject.MainPage;
 import samokatPageObject.OrderFormPage;
 import samokatPageObject.OrderFormRentPage;
@@ -10,7 +8,7 @@ import samokatPageObject.confirmWindow.ApproveOrder;
 import samokatPageObject.confirmWindow.RegisteredOrder;
 
 @RunWith(Parameterized.class)
-public class SuccessOrderTest {
+public class SuccessOrderTest extends BaseTest {
     private final String buttonOrder;
 
     public SuccessOrderTest(String buttonOrder) {
@@ -19,14 +17,11 @@ public class SuccessOrderTest {
 
     @Parameterized.Parameters
     public static Object[] getButtonOrder() {
-        return new Object[] {"Верх", "Низ"};
+        return new Object[]{"Верх", "Низ"};
     }
 
     @Test
     public void checkForSuccessOrder() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
-
         MainPage mainPage = new MainPage(driver);
         OrderFormPage orderFormPage = new OrderFormPage(driver);
         OrderFormRentPage orderFormRentPage = new OrderFormRentPage(driver);
@@ -38,6 +33,5 @@ public class SuccessOrderTest {
         orderFormRentPage.checkSuccessOrder("18.09.2024", "Без комментария");
         approveOrder.clickButtonYes();
         registeredOrder.checkSuccessOrderWindow();
-        driver.quit();
     }
 }
